cdd5eb9238e8562c91b2fd112caa59d1f04ce885
Mathias Dragovic <dragovic.mathias@gmail.com>
2021-12-15 11:22:27 +0100

    API JSON Setup

---
+++ js/factory/recipesFactory.js
@@ -0,0 +1,9 @@
+function recipeFactory(data) {
+ const { id, name, servings, ingredients, time, description, appliance, ustensils } = data;
+
+ function getRecipeCardDOM() {
+
+     return (article);
+ }
+ return { getRecipeCardDOM }
+}
