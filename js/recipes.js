3a7e417b68aa2fa8070a49b6046ff041319e79e7
Mathias Dragovic <dragovic.mathias@gmail.com>
2022-03-16 21:03:57 +0100

    Revert "Récupérer les ingrédients/Appliances/Ustensils"
    This reverts commit bb7aa54152fc0e868727a05a7e0437cc96c89621.

--- js/recipes.js
+++ js/recipes.js
@@ -1,11 +1,6 @@
 /*--------- DOM ELEMENTS ---------*/
 let theRecipes;
 
-let ingredientsArray = [];
-let appliancesArray = [];
-let ustensilsArray = [];
-
-
 /*--------- EVENTS ---------*/
 
 /*--------- FUNCTIONS ---------*/
@@ -20,39 +15,10 @@ const fetchRecipes = async () => {
     });
 };
 
-const keywordDisplay = async () => {
-    await fetchRecipes();
-
-    theRecipes.forEach((recipe) => {
-        /* LES INGREDIENTS */
-        //Recuperer les ingredients des recettes
-        recipe.ingredients.forEach((lesIngredients) => {
-            ingredientsArray.push(lesIngredients.ingredient);
-        });
-        /* LES APPAREILS */
-        //Recuperer les appareils des recettes
-        appliancesArray.push(recipe.appliance);
-
-        /* LES USTENSILES */
-        //Recuperer les ustensiles des recettes
-        recipe.ustensils.forEach((lesUstensiles) => {
-            ustensilsArray.push(lesUstensiles);
-        });
-    });
-    // Remove duplicates from ingredientsArray into uniqueIngredientsArray
-    let uniqueIngredientsArray = [...new Set(ingredientsArray)];
-    // Remove duplicates from appliancesArray into uniqueAppliancesArray
-    let uniqueAppliancesArray = [...new Set(appliancesArray)];
-    // Remove duplicates from ustensilsArray into uniqueUstensilsArray
-    let uniqueUstensilsArray = [...new Set(ustensilsArray)];
-    /* REMOVE */
-    console.log(uniqueIngredientsArray);
-    console.log(uniqueAppliancesArray);
-    console.log(uniqueUstensilsArray);
-}
-
 const recipesDisplay = async () => {
     await fetchRecipes();
+
+    console.log(theRecipes);
 
     const recipesSection = document.querySelector("#resultats");
 
@@ -61,8 +27,7 @@ const recipesDisplay = async () => {
         const recipeCardDOM = recipeModel.getRecipeCardDOM();
         recipesSection.appendChild(recipeCardDOM);
     });
+
 }
 
-
-keywordDisplay();
 recipesDisplay();
